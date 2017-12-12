02b117f435f8f29f77dd4be6644a3da814fcd591
joelmfonseca <joel.fonseca@epfl.ch>
2017-12-12 23:29:12 +0100

    changed few things + added comments

--- project2/plot.py
+++ project2/plot.py
@@ -3,41 +3,37 @@
  
  
 """
-    Plot functions for training loss and validation accuracy of models with different optimizers.
+    Plot functions to create figures.
 """
 
 import matplotlib.pyplot as plt
 
 FIGURE_DIR = 'figures/'
 
-def plot_optim_acc(histories):
+def plot_optim_acc(num_epochs, results):
+    """Generates a plot with validation accuracy for different models."""
 
-    complete_name = 'optim_acc_results'
-    num_epochs = range(1, len(histories[0][1])+1)
-
-    for h in histories:
-        acc = [t[1] for t in h[1]]
-        plt.plot(num_epochs, acc, label=h[0])
+    for r in results:
+        acc = [t[1] for t in r[1]]
+        plt.plot(num_epochs, acc, label=r[0])
 
     plt.xlabel('Epoch')
     plt.title('Validation accuracy')
     leg = plt.legend(loc='lower right', shadow=True)
     leg.draw_frame(False)
-    plt.savefig(FIGURE_DIR + complete_name + '.png')
+    plt.savefig(FIGURE_DIR + 'optim_acc_results.png')
     plt.gcf().clear()
 
-def plot_optim_loss(histories):
+def plot_optim_loss(num_epochs, results):
+    """Generates a plot with training loss for different models."""
 
-    complete_name = 'optim_loss_results'
-    num_epochs = range(1, len(histories[0][1])+1)
-
-    for h in histories:
-        loss = [t[0] for t in h[1]]
-        plt.plot(num_epochs, loss, label=h[0])
+    for r in results:
+        loss = [t[0] for t in r[1]]
+        plt.plot(num_epochs, loss, label=r[0])
 
     plt.xlabel('Epoch')
     plt.title('Training loss')
     leg = plt.legend(loc='upper right', shadow=True)
     leg.draw_frame(False)
-    plt.savefig(FIGURE_DIR + complete_name + '.png')
+    plt.savefig(FIGURE_DIR + 'optim_loss_results.png')
     plt.gcf().clear()
