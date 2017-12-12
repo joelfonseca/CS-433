c1c1c87878b31e950727c09af9a24aded3760745
joelmfonseca <joel.fonseca@epfl.ch>
2017-12-12 23:31:43 +0100

    added docstring

--- project2/utils.py
+++ project2/utils.py
@@ -15,7 +15,8 @@ from parameters import POSTPROCESSING, THRESHOLD_ROAD, CUDA
 from postprocessing import delete_outlier, tetris_shape_cleaner, border_cleaner, region_cleaner, naive_cleaner
 
 def prediction_to_np_patched(img, tensor=True):
-	
+	"""Converts the raw predictions into patched predictions of size 16x16."""
+
 	width = 0
 	height = 0
 
@@ -50,23 +51,26 @@ def prediction_to_np_patched(img, tensor=True):
 
 						
 
-	#print(new_img)
+
 	if POSTPROCESSING:
 		delete_outlier(new_img, 16)
 		tetris_shape_cleaner(new_img, 16)
 		#border_cleaner(new_img, 16)
 		region_cleaner(new_img, 16)
 		#naive_cleaner(new_img, 16)
-	#print(new_img)
 
 	return new_img
 
 
 def patched_to_submission_lines(img, img_number):
+	"""Creates the lines for the submission file for an image."""
+
 	width = int(img.shape[0] / 16)
 	height = int(img.shape[1] / 16)
+
 	for w in range(width):
 		for h in range(height):
+
 			if img[h*16, w*16] == 1:
 				label = 1
 			else:
@@ -75,17 +79,22 @@ def patched_to_submission_lines(img, img_number):
 			yield ("{:03d}_{}_{},{}".format(img_number, w*16, h*16, label))
 
 
+def img_float_to_uint8(img):
+	"""Converts an image float into uint8 format."""
+
+	rimg = img - numpy.min(img)
+	if numpy.max(rimg) == 0:
+		return rimg.astype(numpy.uint8)
+	else:
+		return (rimg / numpy.max(rimg) * 255).round().astype(numpy.uint8)
+
 def concatenate_images(img, gt_img):
-	def img_float_to_uint8(img):
-		rimg = img - numpy.min(img)
-		if numpy.max(rimg) == 0:
-			return rimg.astype(numpy.uint8)
-		else:
-			return (rimg / numpy.max(rimg) * 255).round().astype(numpy.uint8)
+	"""Concatenates an image along with its corresponding groundthruth image."""
 
 	nChannels = len(gt_img.shape)
 	w = gt_img.shape[0]
 	h = gt_img.shape[1]
+
 	if nChannels == 3:
 		cimg = numpy.concatenate((img, gt_img), axis=1)
 	else:
@@ -96,6 +105,7 @@ def concatenate_images(img, gt_img):
 		gt_img_3c[:,:,2] = gt_img8
 		img8 = img_float_to_uint8(img)
 		cimg = numpy.concatenate((img8, gt_img_3c), axis=1)
+
 	return cimg
 
 def build_k_indices(data_and_targets, k_fold, seed=1):
@@ -110,6 +120,7 @@ def build_k_indices(data_and_targets, k_fold, seed=1):
     return np.array(k_indices)
 
 def train_valid_split(train_loader, ratio, seed):
+	"""Splits the data into training and validation sets based on the ratio passed in argument."""
 
 	data = []
 	targets = []
@@ -142,6 +153,7 @@ def train_valid_split(train_loader, ratio, seed):
 	return train_data, train_targets, valid_data, valid_targets
 
 def snapshot(saved_model_dir, run_time, run_name, best, state_dict):
+	"""Saves the model state."""
 	
 	# Write the full name
 	if best :
