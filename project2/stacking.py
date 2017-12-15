0a24068f06f073b2c0279c7f5f4e02a4b4d96c03
joelmfonseca <joel.fonseca@epfl.ch>
2017-12-15 20:24:40 +0100

    corrected non-CUDA code (not checked yet)

--- project2/stacking.py
+++ project2/stacking.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
                 X_train = np.r_[create_input_regr(Variable(data).cuda(), models)]
                 y_train = np.r_[Variable(target, volatile=True).cuda().data.view(-1).cpu().numpy()]
             else:
-                X_train = np.r_[X_train, create_input_regr(Variable(data), models)]
+                X_train = np.r_[create_input_regr(Variable(data), models)]
                 y_train = np.r_[Variable(target, volatile=True).data.view(-1).numpy()]
         else:
             
