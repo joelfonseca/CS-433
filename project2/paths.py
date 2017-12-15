36ddf09a356f9232213d751379863360b426f4c7
joelmfonseca <joel.fonseca@epfl.ch>
2017-12-15 11:53:17 +0100

    created new file containing all paths used

---
+++ project2/paths.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+ 
+"""
+    Contains the different paths to the directories used for the project.
+"""
+
+PREDICTION_TEST_DIR = 'predictions_test/'
+SAVED_MODEL_DIR = 'saved_models/'
+FIGURE_DIR = 'figures/'
