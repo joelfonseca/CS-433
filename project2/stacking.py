a998b3994205b9b31a6e1c374fef8e1f0058c53f
Gangstere44 <lavilleq@gmail.com>
2017-12-19 17:26:10 +0100

    remove useless cuda

--- project2/stacking.py
+++ project2/stacking.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
             
             if CUDA:
                 X_train = np.r_[create_input_regr(Variable(data).cuda(), models)]
-                y_train = np.r_[Variable(target, volatile=True).cuda().data.view(-1).cpu().numpy()]
+                y_train = np.r_[Variable(target, volatile=True).data.view(-1).numpy()]
             else:
                 X_train = np.r_[create_input_regr(Variable(data), models)]
                 y_train = np.r_[Variable(target, volatile=True).data.view(-1).numpy()]
@@ -50,7 +50,7 @@ if __name__ == '__main__':
             
             if CUDA:
                 X_train = np.r_[X_train, create_input_regr(Variable(data).cuda(), models)]
-                y_train = np.r_[y_train, Variable(target, volatile=True).cuda().data.view(-1).cpu().numpy()]
+                y_train = np.r_[y_train, Variable(target, volatile=True).data.view(-1).numpy()]
             else:
                 X_train = np.r_[X_train, create_input_regr(Variable(data), models)]
                 y_train = np.r_[y_train, Variable(target, volatile=True).data.view(-1).numpy()]
